41c5ef19aa5103ddfa5b8ac1ee698ee7b792036f
kereeshka999 <j6452316@gmail.com>
2025-07-26 13:29:43 +0500

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,11 @@
+-- public.киберстаршаки определение
+
+-- Drop table
+
+-- DROP TABLE public.киберстаршаки;
+
+CREATE TABLE public.киберстаршаки (
+	старшаки varchar NULL,
+	катягория varchar NULL,
+	участники varchar NULL
+);
